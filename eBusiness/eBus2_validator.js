5392a2357eea98f54b01f3a8ba5f3259e831d81f
Ali Olden <117324601@umail.ucc.ie>
2018-02-27 16:12:28 +0000

    update

--- eBusiness/eBus2_validator.js
+++ eBusiness/eBus2_validator.js
@@ -1,26 +1,28 @@
-/* global $*/
+/* global $ */
+	function validateDetails(){
+	    
+	    var pin;
+	    
+	    pin = document.getElementById("user_pin").value;
+	    
+	    if (pin == ""){
+	        alert("Please enter your PIN");
+	    }
+	    else if (String(pin).length < 4){
+	        alert("Please make sure your PIN is accurate");
+	    }
+	    else{
+	        enablebtnPurchase();
+	    }
+	    
+	}
+	
 
-function validateDetails(){
-    
-    var pin;
-    
-    pin = document.getElementById ("User_pin").value;
-    
-    if(pin==""){
-        alert("Please enter your PIN")
-    }
-    else if (String(pin).length<4){
-        alert ("Please make sure your pin is accurate");
-    }
-    else{
-        enablebtnPurchase();
-    }
-}
+	function enablebtnPurchase(){
+	    $('#btnPurchase').prop('disabled', false);
+	}
+	
 
-function enablebtnPurchase(){
-    $('#btnPurchase').prop('disabled',false);
-}
-
-function disablePurchase(){
-    $('#bntPurchase').prop('disabled',true);
-}
+	function disablebtnPurchase() {
+	    $('#btnPurchase').prop('disabled', true);
+	}
